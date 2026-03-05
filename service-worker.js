d80f6cdda63c33f23048988b64d2034868fc01b0
Jatin Kr. Koli <kolijatin768@gmail.com>
2026-03-05 23:16:15 +0530

    Refactor service worker to exclude Vercel insights

--- service-worker.js
+++ service-worker.js
@@ -37,10 +37,14 @@ self.addEventListener("activate", event => {
 
 // Fetch Event - Serve from cache if offline
 self.addEventListener("fetch", event => {
+  if (event.request.url.includes('_vercel/insights')) {
+    return;
+  }
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
     })
   );
 });
+
 
