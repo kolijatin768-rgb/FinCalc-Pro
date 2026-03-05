853745512ee7649b5e763a00211485bce4e921ce
Jatin Kr. Koli <kolijatin768@gmail.com>
2026-03-05 20:59:59 +0530

    Update service worker for new caching strategy

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,42 @@
-const CACHE_NAME = "fincalc-cache-v1";
+const CACHE_NAME = "fincalc-pro-v1";
 
-const urlsToCache = [
+const ASSETS = [
   "./",
   "./index.html",
-  "./style.css",
-  "./script.js",
-  "./manifest.json"
+  "./UI.css",
+  "./Logics.js",
+  "./manifest.json",
+  "./favicon.svg",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
